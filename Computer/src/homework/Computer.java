923ed9b0b88fab6d7ab52b409f09a7168d045c59
qaMilord <rustamw888@gmail.com>
2022-09-05 02:33:20 +0300

    first commit

---
+++ Computer/src/homework/Computer.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class Computer {
+
+  protected String name = "IBM";
+
+  public void on() {
+    System.out.println(name + " on");
+  }
+
+  public void off() {
+    System.out.println(name + " off");
+  }
+}
---
+++ Computer/src/homework/Main.java
@@ -0,0 +1,12 @@
+package homework;
+
+import homework.Computer;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Computer computer = new Computer();
+    computer.on();
+    computer.off();
+  }
+}
